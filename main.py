c4b9e443d16774d0eef27696d184d093e1b7a9fb
BlackBond <blackbond@i.ua>
2023-02-08 15:25:50 +0200

    Написан пустой текст цикла pygame

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pygame
 
-pygame.init()
+
+Size_game = (1000, 800)
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((Size_game))
+
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
+
+run_game()
