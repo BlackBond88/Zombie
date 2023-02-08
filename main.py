4e0d628dfcf0bcf703236a6b87c38d7a904e2634
BlackBond <blackbond@i.ua>
2023-02-08 21:57:41 +0200

    Добавлен класс оружия (пистолет)

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from events import *
 
 class Humans:
     def __init__(self):
-        self.health = 100
         self.x = WIDTH / 2
         self.y = HEIGHT / 2
+        self.width = 12
+        self.height = 12
         self.move_left = False
         self.move_right = False
         self.move_up = False
@@ -21,7 +22,21 @@ class Humans:
             self.y -= 1
         if self.move_down:
             self.y += 1
-        pygame.draw.rect(sc, WHITE, (self.x, self.y, 10, 10))
+        pygame.draw.rect(sc, WHITE, (self.x, self.y, self.width, self.height))
+
+
+class Weapons:
+    def __init__(self, human):
+        self.width = 4
+        self.height = 4
+        self.x = human.x + human.width
+        self.y = human.y + human.height / 2 - self.height / 2
+
+
+    def draw(self, human, sc):
+        self.x = human.x + human.width
+        self.y = human.y + human.height / 2 - self.height / 2
+        pygame.draw.rect(sc, RED, (self.x, self.y, self.width, self.height))
 
 
 def run_game():
@@ -30,12 +45,14 @@ def run_game():
     pygame.display.set_caption(NAME)
 
     human = Humans()  # создаем главного героя
+    pistol = Weapons(human)
     while True:
         events(human)
 
         screen.fill(BLACK)
 
         human.draw(screen)  # должно вызываться в отдельном классе
+        pistol.draw(human, screen)
 
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 WHITE = (254, 254, 254)
+RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 
 NAME = 'Zombies in the city'
 WIDTH = 1400
 HEIGHT = 900
-
