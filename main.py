ccf8493b38c45ff8ffad2e5a4537398a633d7b26
BlackBond88 <you@example.com>
2023-02-16 12:56:55 +0200

    Добавлен класс в модуле для работы с курсором мыши, добавлена функция замены курсора на картинку (но не по центру)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from cursor import *
 from events import *
 from weapons import *
 from humans import *
+
 
 
 def run_game():
@@ -11,6 +13,7 @@ def run_game():
     screen = pg.display.set_mode((WIDTH, HEIGHT))   # главный экран игры
     pg.display.set_caption(NAME)
 
+    my_mouse = Mouse()                              # создаем свой курсор мышки
     human = Humans()                                # создаем главного героя
     pistol = Weapons(human)                         # создаем оружие (пистолет)
 
@@ -20,6 +23,7 @@ def run_game():
 
         screen.fill(BLACK)          # заливает экран черным цветом
 
+        my_mouse.draw(screen)       # прорисовка курсора
         human.draw(screen)          # прорисовка главного героя
         # pistol.draw(human, screen)  # прорисовка оружия
 
