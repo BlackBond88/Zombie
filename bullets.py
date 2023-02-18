aec086c0625c121aa460c80a1176622820b0d5b7
BlackBond <blackbond@i.ua>
2023-02-18 20:21:05 +0200

    Добавлен список пуль и ее удаление если покидает экран. Изменена начальная позиция пули. Добавлено ограничение в движении

--- bullets.py
+++ bullets.py
@@ -1,20 +1,39 @@
 import pygame as pg
+from settings import *
 from math import pi, cos, sin
 
 
 class Bullets:
+    """
+    Класс, отвечающий за пули
+    """
     def __init__(self, human):
-        self.x = human.x
-        self.y = human.y
-        self.bullet_image = pg.image.load('Images/bullet.png')
         self.angle = human.angle - 180
+        # Пересчитываем координаты вылета пули относительно положения главного героя
+        self.x = human.x - 59 * cos(self.angle * pi / 180) - 12 * cos(self.angle * pi / 180 - 90)
+        self.y = human.y + 59 * sin(self.angle * pi / 180) + 12 * sin(self.angle * pi / 180 - 90)
+
+        self.bullet_image = pg.image.load('Images/bullet.png')
+        self.delete = False
+        self.human_class = human
 
     def drow(self, screen):
-        bullet_image_rotate = pg.transform.rotate(self.bullet_image, self.angle)
-        rect_bullet = bullet_image_rotate.get_rect(center=(self.x, self.y))
+        """
+        Функция прорисовки и движения пули
+        """
+        bullet_image_rotate = pg.transform.rotate(self.bullet_image, self.angle)    # поворачиваем оригинал
+        rect_bullet = bullet_image_rotate.get_rect(center=(self.x, self.y))         # область с координатами в центре
         screen.blit(bullet_image_rotate, rect_bullet)
 
-        self.angle_rota = self.angle * pi / 180
+        self.x -= BULLET_SPEED * cos(self.angle * pi / 180)
+        self.y += BULLET_SPEED * sin(self.angle * pi / 180)
 
-        self.x -= 20 * cos(self.angle_rota)
-        self.y += 20 * sin(self.angle_rota)
+        self.test_position()
+
+    def test_position(self):
+        """
+        Если пуля покидает экран - удалеет ее
+        """
+        if self.x > WIDTH or self.x < 0 or self.y > HEIGHT or self.y < 0:
+            self.delete = True
+            self.human_class.delete_bullet()
--- humans.py
+++ humans.py
@@ -1,5 +1,3 @@
-import pygame as pg
-from settings import *
 from bullets import *
 from math import pi, atan2
 
@@ -8,26 +6,30 @@ class Humans:
     """
     Класс, занимающийся главным героем
     """
+
     def __init__(self, image_name, speed, screen):
-        self.x = WIDTH / 2              # координаты главного героя (ГГ) выравниваются по центру экрана игры
+        self.x = WIDTH / 2  # координаты главного героя (ГГ) выравниваются по центру экрана игры
         self.y = HEIGHT / 2
-        self.width = WIDTH_HUMAN        # размеры главного героя
+        self.width = WIDTH_HUMAN  # размеры главного героя
         self.height = HEIGHT_HUMAN
-        self.move_left = False          # параметр для движения главного героя при зажатой кнопке
+        self.move_left = False  # параметр для движения главного героя при зажатой кнопке
         self.move_right = False
         self.move_up = False
         self.move_down = False
-        self.angle = 0                  # угол между мышкой и положением главного героя
-        self.image_human = pg.image.load(image_name)        # загружаем изображение героя
+        self.angle = 0  # угол между мышкой и положением главного героя
+        self.image_human = pg.image.load(image_name)  # загружаем изображение героя
         self.speed = speed
         self.screen = screen
         self.shots = False
+        self.bullet_list = []
 
     def draw(self):
         """
         Функция, отвечающая за движение главного героя
         screen: экран главной игры
         """
+        self.move_ban()
+
         if self.move_left:
             self.x -= self.speed
         if self.move_right:
@@ -44,15 +46,26 @@ class Humans:
         self.screen.blit(image_human_rotate, rect_human)
 
         if self.shots:
-            self.bullet.drow(self.screen)   # отрисовка пули
-
+            for bullet in self.bullet_list:
+                bullet.drow(self.screen)  # отрисовка пули
 
     def shot(self):
         """
         Функция, которая создает пули
         """
         self.shots = True
-        self.bullet = Bullets(self)
+        bullet = Bullets(self)
+        self.bullet_list.append(bullet)
+
+    def delete_bullet(self):
+        """
+        Функция пробегает весь список пуль и удаляет те, которые с меткой Delete
+        """
+        i = 0
+        for bullet in self.bullet_list:
+            if bullet.delete:
+                del self.bullet_list[i]
+            i += 1
 
     def mouse_angle(self):
         """
@@ -64,3 +77,13 @@ class Humans:
         coord_y = mouse_y - self.y
         # вычисляем угол между точками в градусах
         self.angle = (180 / pi) * atan2(coord_x, coord_y) - 90
+
+    def move_ban(self):
+        if self.x > WIDTH:
+            self.move_right = False
+        if self.x < 0:
+            self.move_left = False
+        if self.y > HEIGHT:
+            self.move_down = False
+        if self.y < 0:
+            self.move_up = False
